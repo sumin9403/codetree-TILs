225d6882ce18ec8a16d8a5a38f9044d054861eeb
sumin <109321166+sumin9403@users.noreply.github.com>
2023-11-08 07:30:33 +0900

    Title: 문장 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231107/문장 출력/print-sentence.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    printf("H");
+    printf("e");
+    printf("l");
+    printf("l");
+    printf("o");
+    printf(" ");
+    printf("W");
+    printf("o");
+    printf("r");
+    printf("ld!");
+    
+
+
+    return 0;
+}
